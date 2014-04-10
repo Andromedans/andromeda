26f66b4373250d08aa3e03fe84f7cef118b64da1
Christopher A. Stone <stone@cs.hmc.edu>
2014-04-10 15:09:25 +0200

    Added equivalence functions to typing.mli

--- brazil/typing.mli
+++ brazil/typing.mli
@@ -1,4 +1,16 @@
 
+(** Weak-head-normalization (multi-step) of a term *)
+val whnfs    : Context.t -> Syntax.term -> Syntax.term
+
+(** Weak-head-normalization (multi-step) of a type *)
+val whnfs_ty : Context.t -> Syntax.ty   -> Syntax.ty
+
+(** Algorithmic type equivalence *)
+val equiv_ty : Context.t -> Syntax.ty -> Syntax.ty -> bool
+
+(** Algorithmic term equivalence at a type *)
+val equiv : Context.t -> Syntax.term -> Syntax.term -> Syntax.ty -> bool
+
 (** Synthesize the type of a term *)
 val syn_term : Context.t -> Common.debruijn Input.term -> Syntax.term * Syntax.ty
 
