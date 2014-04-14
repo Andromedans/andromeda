ed47471501abee1c055b34df7102cdcf2389acdb
Andrej Bauer <Andrej.Bauer@andrej.com>
2014-04-14 11:25:21 +0200

    Fancy printing of products and lambdas

--- brazil/print.ml
+++ brazil/print.ml
@@ -18,6 +18,30 @@ let error (loc, err_type, msg) = message (err_type) 1 "%s" msg
 let warning msg = message "Warning" 2 msg
 let debug msg = message "Debug" 3 msg
 
+(** Given a variable [x] and a list of variable names [xs], find a variant of [x] which
+    does not appear in [xs]. *)
+let find_name x xs =
+  (** Split a variable name into base and numerical postfix, e.g.,
+      ["x42"] is split into [("x", 42)]. *)
+  let split s =
+    let n = String.length s in
+    let i = ref (n - 1) in
+      while !i >= 0 && '0' <= s.[!i] && s.[!i] <= '9' do decr i done ;
+      if !i < 0 || !i = n - 1
+      then (s, None)
+      else
+        let k = int_of_string (String.sub s (!i+1) (n - !i - 1)) in
+          (String.sub s 0 (!i+1), Some k)
+  in
+
+  if not (List.mem x xs)
+  then x
+  else
+    let (y, k) = split x in
+    let k = ref (match k with Some k -> k | None -> 0) in
+      while List.mem (y ^ string_of_int !k) xs do incr k done ;
+      y ^ string_of_int !k
+
 (** Print an term, possibly placing parentheses around it. We always
     print things at a given [at_level]. If the level exceeds the
     maximum allowed level [max_level] then the term should be parenthesized.
@@ -55,10 +79,73 @@ let sequence ?(sep="") f lst ppf =
   in
     seq lst
 
+let name x ppf = print ~at_level:0 ppf "%s" x
+
 let universe (u,_) ppf =
   print ~at_level:0 ppf "%s" (Universe.to_string u)
 
-let rec term ?max_level xs (e,_) ppf =
+(** [prod xs x t1 t2 ppf] prints a dependent product using formatter [ppf]. *)
+let rec prod ?max_level xs x t1 t2 ppf =
+  if Syntax.occurrs_ty 0 t2 then
+    let x = find_name x xs in
+      print ?max_level ~at_level:3 ppf "(%s :@ %t) ->@ %t"
+        x 
+        (ty ~max_level:2 xs t1)
+        (ty (x :: xs) t2)
+  else
+    print ?max_level ~at_level:3 ppf "%t ->@ %t"
+      (ty ~max_level:2 xs t1)
+      (ty (Input.anonymous :: xs) t2)
+
+(** [name_prod xs x e1 e2 ppf] prints a dependent product name using formatter [ppf]. *)
+and name_prod ?max_level xs x e1 e2 ppf =
+  if Syntax.occurrs 0 e2 then
+    let x = find_name x xs in
+      print ?max_level ~at_level:3 ppf "(%s :@ %t) ->@ %t"
+        x 
+        (term ~max_level:4 xs e1)
+        (term ~max_level:3 (x :: xs) e2)
+  else
+    print ?max_level ~at_level:3 ppf "%t ->@ %t"
+      (term ~max_level:4 xs e1)
+      (term ~max_level:3 (Input.anonymous :: xs) e2)
+
+(** [lambda xs x t u e ppf] prints a lambda abstraction using formatter [ppf]. *)
+and lambda xs x t u e ppf =
+  let rec collect xs y ys t e =
+    let y =
+      if Syntax.occurrs_ty 0 u || Syntax.occurrs 0 e
+      then find_name y xs
+      else Input.anonymous
+    in
+      match fst e with
+        | Syntax.Lambda (z, t', u, e') ->
+          if Syntax.equal_ty (Syntax.shift_ty 1 t) t'
+          then collect (y::xs) z (y::ys) t' e'
+          else (y::xs, y::ys, e)
+        | _ -> 
+          (y::xs, y::ys, e)
+  in
+  let rec abstraction xs x t u e ppf =
+    if !annotate then
+      print ~max_level:4 ppf "(%s :@ %t) =>%t@ %t"
+        x
+        (ty ~max_level:4 xs t)
+        (ty ~max_level:4 (x::xs) u)
+        (term ~max_level:4 (x::xs) e)
+    else
+      let (xs', ys, e) = collect xs x [] t e in
+      let ys = List.rev ys in
+        print ~at_level:0 ppf "(%t :@ %t)@ "
+          (sequence name ys)
+          (ty ~max_level:4 xs t) ;
+        match fst e with
+          | Syntax.Lambda (x, t, u, e) -> abstraction xs' x t u e ppf
+          | _ -> print ~at_level:0 ppf "=>@ %t" (term ~max_level:4 xs' e)
+  in          
+    print ~at_level:4 ppf "fun %t" (abstraction xs x t u e)
+      
+and term ?max_level xs (e,_) ppf =
   let print' = print
   and print ?at_level = print ?max_level ?at_level ppf in
     match e with
@@ -82,17 +169,17 @@ let rec term ?max_level xs (e,_) ppf =
           (ty ~max_level:4 xs t)
 
       | Syntax.Lambda (x, t, u, e) ->
-        print ~at_level:4 "fun (%s : %t) =>%t@ %t"
-          x
-          (ty ~max_level:4 xs t)
-          (annot (ty ~max_level:4 (x::xs) u))
-          (term ~max_level:4 (x::xs) e)
+        lambda xs x t u e ppf
 
       | Syntax.App ((x, t, u), e1, e2) ->
         print ~at_level:1 "%t@,%t@ %t"
           (term ~max_level:1 xs e1)
           (annot ~prefix:" @"
-             (fun ppf -> print' ~max_level:4 ppf "%s :@ %t .@ %t" x (ty ~max_level:4 xs t) (ty ~max_level:4 (x::xs) u)))
+             (fun ppf -> print' ~max_level:4 ppf "%s :@ %t .@ %t"
+                           x
+                           (ty ~max_level:4 xs t)
+                           (ty ~max_level:4 (x::xs)
+                           u)))
           (term ~max_level:0 xs e2)
 
       | Syntax.UnitTerm -> print ~at_level:0 "()"
@@ -123,23 +210,20 @@ let rec term ?max_level xs (e,_) ppf =
 
       | Syntax.NameUnit -> print ~at_level:0 "unit"
 
-      | Syntax.NameProd (_alpha, _beta, x, e1, e2) ->
-        print ~at_level:3 "(%s : %t) ->@ %t"
-          x
-          (term ~max_level:4 xs e1)
-          (term ~max_level:3 (x::xs) e2)
+      | Syntax.NameProd (_, _, x, e1, e2) ->
+        name_prod xs x e1 e2 ppf
 
       | Syntax.NameUniverse u ->
         print ~at_level:1 "Universe %t"
           (universe u)
 
-      | Syntax.NamePaths (_alpha, e1, e2, e3) ->
+      | Syntax.NamePaths (_, e1, e2, e3) ->
         print ~at_level:2 "@[<hv 2>%t@ =%t@ %t@]"
           (term ~max_level:1 xs e2)
           (annot (term ~max_level:4 xs e1))
           (term ~max_level:1 xs e3)
 
-      | Syntax.NameId (_alpha, e1, e2, e3) ->
+      | Syntax.NameId (_, e1, e2, e3) ->
         print ~at_level:2 "@[<hv 2>%t@ ==%t@ %t@]"
           (term ~max_level:1 xs e2)
           (annot (term ~max_level:4 xs e1))
@@ -155,7 +239,7 @@ and ty ?max_level xs (t,_) ppf =
 
       | Syntax.El (u, e) ->
           if (!annotate) then
-            print "El(%t,%t)" (universe u) (term ~max_level:4 xs e)
+            print "El(%t, %t)" (universe u) (term ~max_level:4 xs e)
           else
             print "%t"
               (term ?max_level xs e)
@@ -164,10 +248,7 @@ and ty ?max_level xs (t,_) ppf =
         print ~at_level:0 "unit"
 
       | Syntax.Prod (x, t1, t2) ->
-        print ~at_level:3 "(%s : %t) ->@ %t"
-          x
-          (ty ~max_level:4 xs t1)
-          (ty ~max_level:3 (x::xs) t2)
+        prod xs x t1 t2 ppf
 
       | Syntax.Paths (t, e1, e2) ->
         print ~at_level:2 "@[<hv 2>%t@ =%t %t@]"
