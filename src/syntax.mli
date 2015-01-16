e52b2deeb544477498f6fd0e9842ee54188de711
Matija Pretnar <matija@pretnar.info>
2015-01-16 12:07:04 +0100

    Remove Syntax implementation
    Since the implementation merely repeats the
    interface and defines no values, there is no harm
    in deleting it.

--- src/syntax.mli
+++ src/syntax.mli
@@ -1,16 +1,19 @@
-(** Abstract syntax of computations and expressions. *)
+(** Desugared input *)
 
-(** Bound variables *)
+(** Bound variables - represented by de Bruijn indices *)
 type bound = int
 
+(** Desugared expressions *)
 type expr = expr' * Location.t
  and expr' =
    | Name of Name.t
    | Bound of bound
    | Type
 
+(** Desugared types - indistinguishable from expressions  *)
 and ty = expr
 
+(** Desugared computations *)
 and comp = comp' * Location.t
 and comp' = 
   | Return of expr
@@ -22,11 +25,12 @@ and comp' =
   | Eq of expr * comp
   | Refl of expr
 
+(** Desugared toplevel commands *)
 type toplevel = toplevel' * Location.t
 and toplevel' =
   | Parameter of Name.t list * comp (** introduce parameters into the context *)
   | TopLet of Name.t * comp (** global let binding *)
   | TopCheck of comp (** infer the type of a computation *)
-  | Quit
-  | Help
-  | Context
+  | Quit (** quit the toplevel *)
+  | Help (** print help *)
+  | Context (** print the current context *)
