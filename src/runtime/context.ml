b2cd66f5e3ea6fc74c0ac40df9b07e71c9f5145e
Matija Pretnar <matija@pretnar.info>
2015-01-21 20:38:49 +0100

    Remove Context.find_name
    This was replaced by Name.find_name long time ago.

--- src/runtime/context.ml
+++ src/runtime/context.ml
@@ -45,29 +45,6 @@ let add_fresh x t ctx =
 let add_bound x v ctx =
   { ctx with bound = (x, v) :: ctx.bound }
 
-(** Given a variable [x] and a context [ctx], find a variant of [x] which
-    does not appear in [ctx]. *)
-let find_name x ctx =
-  (** Split a variable name into base and numerical postfix, e.g.,
-      ["x42"] is split into [("x", 42)]. *)
-  let split s =
-    let n = String.length s in
-    let i = ref (n - 1) in
-      while !i >= 0 && '0' <= s.[!i] && s.[!i] <= '9' do decr i done ;
-      if !i < 0 || !i = n - 1
-      then (s, None)
-      else
-        let k = int_of_string (String.sub s (!i+1) (n - !i - 1)) in
-          (String.sub s 0 (!i+1), Some k)
-  in
-  if not (List.mem_assoc x ctx)
-  then x
-  else
-    let (y, k) = split (Name.to_string x) in
-    let k = ref (match k with Some k -> k | None -> 0) in
-      while List.mem_assoc (Name.of_string (y ^ string_of_int !k)) ctx do incr k done ;
-      Name.of_string (y ^ string_of_int !k)
-
 let print ctx ppf =
   let forbidden_names = used_names ctx in
   Print.print ppf "---------@." ;
