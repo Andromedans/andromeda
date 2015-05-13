12bf8aedb8ca2798ceb6a8c990d685a75cedb620
Philipp Haselwarter <philipp@haselwarter.org>
2015-05-13 16:30:46 +0200

    Emacs: Be more careful with dir-locals (fix dired)

--- .dir-locals.el
+++ .dir-locals.el
@@ -10,20 +10,21 @@
 		(tt-project-find-file
 		 (and (boundp 'tt-project-find-file) tt-project-find-file)))
 	    ;; tt tags file
-	    (setq tags-file-name (concat tt-root-directory "TAGS"))
+        (when tt-root-directory
+          (setq tags-file-name (concat tt-root-directory "TAGS"))
 
-	    ;; Setting the compilation directory to tt root. This is
-	    ;; mutually exclusive with the setting of default-directory
-	    ;; below.
-	    (unless tt-project-find-file
-	      (setq compile-command (concat "make -C " tt-root-directory)))
+          ;; Setting the compilation directory to tt root. This is
+          ;; mutually exclusive with the setting of default-directory
+          ;; below.
+          (unless tt-project-find-file
+            (setq compile-command (concat "make -C " tt-root-directory)))
 
-	    ;; Set default directory to tt root ONLY IF variable
-	    ;; tt-project-find-file is non nil. This should remain a
-	    ;; user preference and not be set by default. This setting
-	    ;; is redundant with compile-command above as M-x compile
-	    ;; always CD's to default directory. To enable it add this
-	    ;; to your emacs config: (setq tt-project-find-file t)
-	    (setq default-directory tt-root-directory)))
+          ;; Set default directory to tt root ONLY IF variable
+          ;; tt-project-find-file is non nil. This should remain a
+          ;; user preference and not be set by default. This setting
+          ;; is redundant with compile-command above as M-x compile
+          ;; always CD's to default directory. To enable it add this
+          ;; to your emacs config: (setq tt-project-find-file t)
+          (setq default-directory tt-root-directory))))
       ))
   ))
