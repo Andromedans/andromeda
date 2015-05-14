ca864fc028eb402b8f6b64ccd3d33b4c49357a23
Philipp Haselwarter <philipp@haselwarter.org>
2015-05-14 09:08:26 +0200

    un-break emacs setup some more

--- .dir-locals.el
+++ .dir-locals.el
@@ -16,15 +16,15 @@
           ;; Setting the compilation directory to tt root. This is
           ;; mutually exclusive with the setting of default-directory
           ;; below.
-          (unless tt-project-find-file
-            (setq compile-command (concat "make -C " tt-root-directory)))
+          (if (not tt-project-find-file)
+              (setq compile-command (concat "make -C " tt-root-directory))
 
-          ;; Set default directory to tt root ONLY IF variable
-          ;; tt-project-find-file is non nil. This should remain a
-          ;; user preference and not be set by default. This setting
-          ;; is redundant with compile-command above as M-x compile
-          ;; always CD's to default directory. To enable it add this
-          ;; to your emacs config: (setq tt-project-find-file t)
-          (setq default-directory tt-root-directory))))
+            ;; Set default directory to tt root ONLY IF variable
+            ;; tt-project-find-file is non nil. This should remain a
+            ;; user preference and not be set by default. This setting
+            ;; is redundant with compile-command above as M-x compile
+            ;; always CD's to default directory. To enable it add this
+            ;; to your emacs config: (setq tt-project-find-file t)
+            (setq default-directory tt-root-directory)))))
       ))
   ))
