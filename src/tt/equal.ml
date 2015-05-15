b7ce7ecd5f9e13aac8e31682f59fa0bea78ec0e2
Philipp Haselwarter <philipp@haselwarter.org>
2015-05-15 23:19:26 +0200

    Add a level to equality checks of types
    The types of PVars may be defined at different binding depths.
    Therefore, when instantiating them, we need to drop bindings introduced
    after them, and in fact also their own binding.

--- src/tt/equal.ml
+++ src/tt/equal.ml
@@ -4,7 +4,7 @@
     Pattern matching generates these. *)
 type check =
   | CheckEqual of Tt.term * Tt.term * Tt.ty
-  | CheckEqualTy of Tt.ty * Tt.ty
+  | CheckEqualTy of Tt.ty * Tt.ty * int
   | CheckAlphaEqual of Tt.term * Tt.term
 
 (** Alpha equality *)
@@ -401,6 +401,9 @@ and pattern_match ctx (xts, p) ?t e =
          [p]. However, since [PVar] cannot appear under any binders, [t'] and
          [t''] are equal in the context of [xts]. Thus, we can compare
          the given type [t] to any one of them. *)
+      (* Note that [t'] is at binding level [k] and has to be instantiated with
+         only the variables that have been bound before its introduction. Cut
+         k+1 elements off [es]. *)
       let (x, t') =
         begin try
           (List.nth xts k)
@@ -408,7 +411,7 @@ and pattern_match ctx (xts, p) ?t e =
           Error.impossible "Encountered an unknown pattern variable in Pattern.collect"
         end in
       begin match t with
-      | Some t -> [(k, (e, t'))], [CheckEqualTy (t', t)]
+      | Some t -> [(k, (e, t'))], [CheckEqualTy (t', t, k+1)]
       | None ->
         (** We only get here if the caller of [pattern_match] does not provide
             [t] _and_ we hit a variable as the first pattern. This can happen
@@ -448,7 +451,7 @@ and pattern_match ctx (xts, p) ?t e =
 
     | Pattern.Term (e',t') ->
       begin match t with
-        | Some t -> [], [CheckEqualTy (t, t'); CheckEqual (e', e, t)]
+        | Some t -> [], [CheckEqualTy (t, t', 0); CheckEqual (e', e, t)]
         | None ->
           (** It is unsafe to compare [e'] and [e] for equality when
               the type of [e] is not given. However, it is safe to
@@ -464,7 +467,8 @@ and pattern_match ctx (xts, p) ?t e =
       | [], [] ->
         let xts' = List.rev xts'
         and xts  = List.rev xts in
-        let check_u = [CheckEqualTy (Tt.mk_prod_ty ~loc xts' u', Tt.mk_prod_ty ~loc xts u)]
+        let check_u = [CheckEqualTy (Tt.mk_prod_ty ~loc xts' u',
+                                     Tt.mk_prod_ty ~loc xts u, 0)]
         in [], check_u
       | (x',(pe,t'))::pxets, (x,(e,t))::xets ->
         let pvars_e, checks_e = collect pe ~t:(Tt.instantiate_ty es 0 t) e
@@ -478,12 +482,20 @@ and pattern_match ctx (xts, p) ?t e =
 
   in
 
+  let rec drop n = function
+    | [] -> if n = 0 then Some [] else None
+    | _ :: t -> drop (n-1) t in
+
   let rec bind_pvars ctx k pvars xts es =
     begin match pvars, xts with
       | [], [] -> ctx, es
       | (i,(e,t))::pvars, (x,t')::xts ->
         if i <> k then raise NoMatch else
           begin
+            let lvl = k+1 in
+            let es = match drop lvl es with Some es -> es | None ->
+              let Tt.Ty (_, loc) = t in
+              Error.typing ~loc "de Bruijn encountered in Equal.pattern_match" in
             let t = Tt.instantiate_ty es 0 t in
             let ctx = Context.add_bound x (e,t) ctx in
             bind_pvars ctx (k+1) pvars xts (e::es)
@@ -506,7 +518,10 @@ and pattern_match ctx (xts, p) ?t e =
           if not
               (equal ctx e1 e2 t) then
             raise NoMatch
-        | CheckEqualTy (t1, t2) ->
+        | CheckEqualTy (t1, t2, lvl) ->
+          let es = match drop lvl es with Some es -> es | None ->
+            let Tt.Ty (_, loc) = t1 in
+            Error.typing ~loc "de Bruijn encountered in Equal.pattern_match" in
           let t1 = Tt.instantiate_ty es 0 t1
           and t2 = Tt.instantiate_ty es 0 t2 in
           if not (equal_ty ctx t1 t2) then raise NoMatch
