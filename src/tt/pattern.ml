df19ed3d4face102128f7838b70196c847282c68
Philipp Haselwarter <philipp@haselwarter.org>
2015-05-15 23:22:45 +0200

    Spines are binders. We are doomed?

--- src/tt/pattern.ml
+++ src/tt/pattern.ml
@@ -26,56 +26,56 @@ let rec remove_bound x = function
 
 (** Convert a term [e] of type [t] to a pattern with respect to the
     given bound variables [pvars]. *)
-let rec of_term pvars ((e',loc) as e) t =
+let rec of_term pvars lvl ((e',loc) as e) t =
   let original = pvars, Term (e,t) in
   match e' with
 
   | Tt.Type | Tt.Name _ | Tt.Lambda _ | Tt.Prod _ -> original
 
   | Tt.Bound k ->
-    begin match remove_bound k pvars with
+    begin match remove_bound (k - lvl) pvars with
       | None -> original
       | Some pvars -> pvars, PVar k
     end
 
   | Tt.Spine (e, (xets, u)) ->
-    let rec fold pvars = function
+    let rec fold pvars lvl = function
       | [] -> pvars, true, []
       | (x, (e, t)) :: xets ->
-        let pvars, e = of_term pvars e t in
-        let pvars, all_terms, xets = fold pvars xets in
+        let pvars, e = of_term pvars lvl e t in
+        let pvars, all_terms, xets = fold pvars (lvl+1) xets in
         let all_terms = (match e with Term _ -> all_terms | _ -> false) in
         let xets = (x, (e, t)) :: xets in
         pvars, all_terms, xets
     in
 
     let xts = List.map (fun (x, (_, t)) -> x, t) xets in
-    let pvars, e = of_term pvars e (Tt.ty (Tt.mk_prod ~loc xts u)) in
-    let pvars, all_terms, xets = fold pvars xets in
+    let pvars, e = of_term pvars lvl e (Tt.ty (Tt.mk_prod ~loc xts u)) in
+    let pvars, all_terms, xets = fold pvars 0 xets in
     begin match all_terms, e with
       | true, Term _ -> original
       | _, _ -> pvars, (Spine (e, (xets, u)))
     end
 
   | Tt.Eq (t, e1, e2) ->
-    let pvars, t' = of_ty pvars t in
-    let pvars, e1 = of_term pvars e1 t in
-    let pvars, e2 = of_term pvars e2 t in
+    let pvars, t' = of_ty lvl pvars t in
+    let pvars, e1 = of_term pvars lvl e1 t in
+    let pvars, e2 = of_term pvars lvl e2 t in
     begin match t', e1, e2 with
       | Ty (Term _), Term _, Term _ -> original
       | Ty _, _, _ -> pvars, (Eq (t', e1, e2))
     end
 
   | Tt.Refl (t, e) ->
-    let pvars, t' = of_ty pvars t in
-    let pvars, e = of_term pvars e t in
+    let pvars, t' = of_ty lvl pvars t in
+    let pvars, e = of_term pvars lvl e t in
     begin match t', e with
       | Ty (Term _), Term _ -> original
       | _, _ -> pvars, (Refl (t', e))
     end
 
-and of_ty pvars (Tt.Ty t) : Syntax.bound list * ty =
-  let pvars, t = of_term pvars t Tt.typ in
+and of_ty pvars lvl (Tt.Ty t) : Syntax.bound list * ty =
+  let pvars, t = of_term lvl pvars t Tt.typ in
   pvars, (Ty t)
 
 let rec print_term ?max_level (xs : Name.t list) e ppf =
@@ -140,7 +140,7 @@ let print_pattern ?max_level xs (xts, p) ppf =
 
 let make (xts, (e, t)) =
   let _, pvars = List.fold_left (fun (k, pvars) _ -> (k+1), k :: pvars) (0, []) xts in
-  let pvars, p = of_term pvars e t in
+  let pvars, p = of_term pvars 0 e t in (* XXX [t] can be a PVar *)
   let e = Tt.mk_lambda ~loc:Location.unknown xts e t in
     Print.debug "Created pattern %t from abstraction %t"
       (print_pattern [] (xts, p))
