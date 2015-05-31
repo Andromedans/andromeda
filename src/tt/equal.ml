a933f7dde48e40b1e69c726ff4560ee527cfa38e
Andrej Bauer <Andrej.Bauer@andrej.com>
2015-05-31 13:13:27 +0200

    Make Travis happy – again...

--- src/tt/equal.ml
+++ src/tt/equal.ml
@@ -756,7 +756,7 @@ and inhabit_bracket ctx t =
 
 let rec as_universal_eq ctx t =
   let rec fold ctx xus ys t =
-    let (Tt.Ty (t', loc)) as t = whnf_ty ctx t in
+    let (Tt.Ty (t', loc)) = whnf_ty ctx t in
     match t' with
 
     | Tt.Prod ([], _) ->
