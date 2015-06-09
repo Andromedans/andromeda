b9a61e32ead41ac4f3d6647a2298c23b54188663
Philipp Haselwarter <philipp@haselwarter.org>
2015-06-09 13:55:32 +0200

    Usable emacs mode

--- etc/andromeda.el
+++ etc/andromeda.el
@@ -1,5 +1,6 @@
 (require 'proof)
 
+;; debugging facilities for the andromeda project
 (setq m31-comint-filters
       '((lambda (s)
           (replace-regexp-in-string "^ocd " "(ocd) " s))
@@ -38,35 +39,62 @@
   (comint-send-string (get-buffer-process (current-buffer))
                       "source etc/debug-current\n"))
 
+;; the major mode for writing galactical type theory
+;;;###autoload
 (define-derived-mode andromeda-mode coq-mode "m31")
+;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.m31\\'" . andromeda-mode))
 
-(defun m31-send-current-statement nil
-  (interactive)
-  (save-excursion
-    (comint-send-string
-     (get-buffer-process (get-buffer "*andromeda*"))
-     (concat
-      (replace-regexp-in-string
-       "\n" " "
-       (buffer-substring-no-properties
-        (1+ (search-backward "." 0)) (search-forward "." nil nil 2)))
-      "
-"))))
+(defun m31-get-andromeda-buffer-create nil
+  (get-buffer-create "*andromeda*"))
 
-(defun m31-send-region nil
+;;;###autoload
+(defun m31-send-file (fn)
   (interactive)
-  (save-excursion
-    (comint-send-string
-     (get-buffer-process (get-buffer "*andromeda*"))
-     (concat
-      (replace-regexp-in-string
-       "\n" " "
-       (buffer-substring-no-properties
-        (region-beginning) (region-end)))
-      "
-"))))
-;; (buffer-local-set-key (kbd "C-M-x") 'm31-send-current-statement)
+  (let ((cmd
+         (concat (locate-dominating-file
+                  buffer-file-name
+                  ".dir-locals.el") "andromeda.byte " fn))
+        (compilation-buffer-name-function (lambda (mm) "*andromeda*"))
+        (compilation-scroll-output 'first-error)
+        (compilation-ask-about-save nil)
+        (hist compile-history)
+        (prev-cmd compile-command))
+    (setq m31--current-buffer (current-buffer))
+    (compile cmd)
+    (setq compile-history hist
+          compile-command prev-cmd)
+    (with-current-buffer "*andromeda*"
+      (set
+       (make-local-variable
+        'compilation-finish-functions)
+       '((lambda (buf msg)
+           (let ((c (get-buffer-window m31--current-buffer))
+                 (w (get-buffer-window buf 'visible)))
+             (when w
+               (select-window w t)
+               (when (eobp)
+                 (recenter -1))
+               (select-window c t)))))))))
+
+;;;###autoload
+(defun m31-send-buffer nil
+  "Send the current buffer to Andromeda"
+  (interactive)
+  (m31-send-file buffer-file-name))
+
+;;;###autoload
+(defun m31-send-buffer-up-to-point nil
+  (interactive)
+  (m31-send-file
+   (concat
+    buffer-file-name ":"
+    (int-to-string (line-number-at-pos)))))
+
+(define-key andromeda-mode-map (kbd "C-c C-.") 'm31-send-buffer-up-to-point)
+(define-key andromeda-mode-map (kbd "C-c .") 'm31-send-buffer-up-to-point)
+(define-key andromeda-mode-map (kbd "C-c C-b") 'm31-send-buffer)
+(define-key andromeda-mode-map (kbd "C-c C-l") 'm31-send-buffer)
 
 
 (provide 'andromeda)
