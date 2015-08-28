ca266684c54d2618897b126d72709381d414e538
Andrej Bauer <Andrej.Bauer@andrej.com>
2015-08-28 08:20:25 +0200

    Simplify .dir-locals.el
    This now works under my Emacs and hopefull does not break
    everybody else's Emacs.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,34 +1,6 @@
 ;; Support for compiling in subdirectories from Emacs. Adapted from Coq source.
 ((nil
-  . ((eval
-      . (progn
-          ;; root directory (ending with slash)
-          (let ((m31-root-directory
-                 (when buffer-file-name
-                   (locate-dominating-file
-                    buffer-file-name
-                    ".dir-locals.el")))
-                (m31-project-find-file
-                 (and (boundp 'm31-project-find-file) m31-project-find-file)))
-
-            ;; m31 tags file
-            (when m31-root-directory
-              (setq tags-file-name (concat m31-root-directory "TAGS"))
-
-              ;; Setting the compilation directory to m31 root. This is
-              ;; mutually exclusive with the setting of default-directory
-              ;; below.
-              (if (not m31-project-find-file)
-                  (setq compile-command (concat "make -C " m31-root-directory))
-
-                ;; Set default directory to m31 root ONLY IF variable
-                ;; m31-project-find-file is non nil. This should remain a
-                ;; user preference and not be set by default. This setting
-                ;; is redundant with compile-command above as M-x compile
-                ;; always CD's to default directory. To enable it add this
-                ;; to your emacs config: (setq m31-project-find-file t)
-                (setq default-directory m31-root-directory)))
-            (setq m31-executable (concat m31-root-directory "andromeda.byte")))))))
+  (eval . (setq default-directory (locate-dominating-file buffer-file-name ".dir-locals.el"))))
  (tuareg-mode
   (show-trailing-whitespace . t))
  (m31-mode
