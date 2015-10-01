19b519c34f7a632e1a4a572a71659a9e3e8865c4
Andrej Bauer <Andrej.Bauer@andrej.com>
2015-10-01 12:39:08 +0200

    Still trying to fix .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,23 +5,20 @@
           ;; root directory (ending with slash)
           (let ((m31-root-directory
                  (when buffer-file-name
-                   (locate-dominating-file
-                    buffer-file-name
-                    ".dir-locals.el")))
+                   (locate-dominating-file buffer-file-name ".dir-locals.el")))
                 (m31-project-find-file
                  (and (boundp 'm31-project-find-file) m31-project-find-file)))
 
             ;; m31 tags file
             (when m31-root-directory
               (setq tags-file-name (concat m31-root-directory "TAGS"))
-
+              (add-to-list 'compilation-search-path m31-root-directory)
               ;; Setting the compilation directory to m31 root. This is
               ;; mutually exclusive with the setting of default-directory
               ;; below.
               (if (not m31-project-find-file)
-                  (setq compile-command (concat "make -C " m31-root-directory))
-              (add-to-list (quote compilation-search-path) andromeda-root-directory))
-              ))
+                  (setq compile-command (concat "make -C " m31-root-directory)))
+              )
             (setq m31-executable (concat m31-root-directory "andromeda.byte")))))))
  (tuareg-mode
   (show-trailing-whitespace . t))
