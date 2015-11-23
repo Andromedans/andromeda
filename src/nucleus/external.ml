5328fae3f8fbef6c556b2957771acf85eeb5c053
Philipp Haselwarter <philipp@haselwarter.org>
2015-11-23 17:04:50 +0100

    Add [time] to External
    [time x y] prints the time elapsed between receiving x and y. It
    discards x and returns y.

--- src/nucleus/external.ml
+++ src/nucleus/external.ml
@@ -1,12 +1,24 @@
 (** Predefined external values. *)
 
-let externals = [
-  ("print",
-   Value.Closure (fun v ->
-                  Format.printf "%t@\n" (Value.print_value [] v) ;
-                  Value.return (Value.Tag (Name.make "tt", []))
-                 ))
-]
+let externals =
+  [
+    ("print",
+     Value.Closure (fun v ->
+         Format.printf "%t@\n" (Value.print_value [] v) ;
+         Value.return (Value.Tag (Name.make "tt", []))
+       )) ;
+    ("time",
+     let time = ref 0. in
+     Value.Closure (fun _ ->
+         time := Sys.time ();
+         Value.return
+           (Value.Closure
+              (fun v ->
+                 let t0 = !time in let t = Sys.time () in
+                 Format.printf "Time used: %fs\n" (t -. t0) ;
+                 Value.return v))
+       ));
+  ]
 
 
 let lookup s =
