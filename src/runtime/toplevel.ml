9d9537fcb146ae97ba5d20c5c0c351c77ef296fb
Gaetan Gilbert <gaetan.gilbert@ens-lyon.fr>
2016-03-31 11:43:29 +0200

    Better exception packing
    Before we caught all exceptions, then reraised the ones we don't know about. This destroys the stack trace, so if we got a `Not_found` it was hard to find.

--- src/runtime/toplevel.ml
+++ src/runtime/toplevel.ml
@@ -24,50 +24,42 @@ let print_error err ppf =
 let print_located_error {Location.thing=err; loc} ppf =
   Format.fprintf ppf "%t:@ %t" (Location.print loc) (print_error err)
 
-let reraise runtime = function
-  | Runtime.Error {Location.thing=err; loc} ->
-     let penv = Runtime.get_penv runtime in
-     raise (Error (Location.locate (RuntimeError (err, penv)) loc))
-  | Jdg.Error {Location.thing=err; loc} ->
-     let penv = Runtime.get_penv runtime in
-     raise (Error (Location.locate (JdgError (err, penv)) loc))
-  | Ulexbuf.Error {Location.thing=err; loc} ->
-    raise (Error (Location.locate (ParserError err) loc))
-  | Desugar.Error {Location.thing=err; loc} ->
-    raise (Error (Location.locate (DesugarError err) loc))
-  | exn -> raise exn
+let wrap f state =
+  try f state
+  with
+    | Runtime.Error {Location.thing=err; loc} ->
+       let penv = Runtime.get_penv state.runtime in
+       raise (Error (Location.locate (RuntimeError (err, penv)) loc))
+    | Jdg.Error {Location.thing=err; loc} ->
+       let penv = Runtime.get_penv state.runtime in
+       raise (Error (Location.locate (JdgError (err, penv)) loc))
+    | Ulexbuf.Error {Location.thing=err; loc} ->
+      raise (Error (Location.locate (ParserError err) loc))
+    | Desugar.Error {Location.thing=err; loc} ->
+      raise (Error (Location.locate (DesugarError err) loc))
 
 (** Evaluation of toplevel computations *)
-let exec_cmd ~quiet c {desugar;typing;runtime} =
-  try
-    let desugar, c = Desugar.toplevel  ~basedir:Filename.current_dir_name desugar c in
-    let typing = Mlty.infer typing c in
-    let comp = Eval.toplevel ~quiet c in
-    let (), runtime = Runtime.exec comp runtime in
-    {desugar;typing;runtime}
-  with
-    | exn -> reraise runtime exn
+let exec_cmd ~quiet c = wrap (fun {desugar;typing;runtime} ->
+  let desugar, c = Desugar.toplevel  ~basedir:Filename.current_dir_name desugar c in
+  let typing = Mlty.infer typing c in
+  let comp = Eval.toplevel ~quiet c in
+  let (), runtime = Runtime.exec comp runtime in
+  {desugar;typing;runtime})
 
-let exec_interactive state =
-  try
-    let cmd = Lexer.read_toplevel Parser.commandline () in
-    exec_cmd ~quiet:false cmd state
-  with
-    | exn -> reraise state.runtime exn
+let exec_interactive = wrap (fun state ->
+  let cmd = Lexer.read_toplevel Parser.commandline () in
+  exec_cmd ~quiet:false cmd state)
 
-let use_file ~fn ~quiet {desugar;typing;runtime} =
-  try
-    let desugar, cmds = Desugar.file desugar fn in
-    let typing = List.fold_left Mlty.infer typing cmds in
-    let comp =
-      List.fold_left
-        (fun m cmd -> Runtime.top_bind m (fun () -> Eval.toplevel ~quiet cmd))
-        (Runtime.top_return ()) cmds
-    in
-    let (), runtime = Runtime.exec comp runtime in
-    {desugar;typing;runtime}
-  with
-    | exn -> reraise runtime exn
+let use_file ~fn ~quiet = wrap (fun {desugar;typing;runtime} ->
+  let desugar, cmds = Desugar.file desugar fn in
+  let typing = List.fold_left Mlty.infer typing cmds in
+  let comp =
+    List.fold_left
+      (fun m cmd -> Runtime.top_bind m (fun () -> Eval.toplevel ~quiet cmd))
+      (Runtime.top_return ()) cmds
+  in
+  let (), runtime = Runtime.exec comp runtime in
+  {desugar;typing;runtime})
 
 let initial =
   let cmds = Lexer.read_string Parser.file Predefined.definitions in
