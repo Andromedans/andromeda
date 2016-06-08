05f8617a46b8d361af293d87a2b0ef52e1a80233
Christopher A. Stone <stone@cs.hmc.edu>
2016-06-08 21:10:58 +0200

    More commenting (and a little cleanup)
    Comments in predefined.mli and predefined.ml
    Slightly changed the interface to be more logical.

--- src/runtime/predefined.ml
+++ src/runtime/predefined.ml
@@ -4,6 +4,10 @@ type coercible =
   | Convertible of Jdg.eq_ty
   | Coercible of Jdg.term
 
+(************************)
+(* Built-in Definitions *)
+(************************)
+
 let name_alpha = Name.make (Name.greek 0)
 
 let predefined_aml_types = let open Input in
@@ -52,18 +56,26 @@ let predefined_bound_names =
 let definitions = List.concat [predefined_aml_types; predefined_ops; predefined_bound]
 
 
-let list_nil = Runtime.mk_tag Name.Predefined.nil []
+(************************)
+(* AML list <-> ML list *)
+(************************)
 
+let list_nil        = Runtime.mk_tag Name.Predefined.nil []
 let list_cons v lst = Runtime.mk_tag Name.Predefined.cons [v; lst]
 
 let rec mk_list = function
-  | [] -> list_nil
+  | []      -> list_nil
   | x :: xs -> list_cons x (mk_list xs)
 
 let as_list ~loc v =
   match Runtime.as_list_opt v with
   | Some lst -> lst
   | None -> Runtime.(error ~loc (ListExpected v))
+
+
+(****************************)
+(* AML option <-> ML option *)
+(****************************)
 
 let from_option = function
   | Some v -> Runtime.mk_tag Name.Predefined.some [v]
@@ -75,6 +87,12 @@ let as_option ~loc = function
   | (Runtime.Term _ | Runtime.Closure _ | Runtime.Handler _ | Runtime.Tag _ | Runtime.Tuple _ |
      Runtime.Ref _ | Runtime.String _) as v ->
      Runtime.(error ~loc (OptionExpected v))
+
+
+
+(**********************************)
+(* AML coercible <-> ML coercible *)
+(**********************************)
 
 let as_coercible ~loc = function
   | Runtime.Tag (t, []) when Name.eq_ident t Name.Predefined.notcoercible ->
@@ -90,12 +108,20 @@ let as_coercible ~loc = function
      Runtime.Ref _ | Runtime.String _) as v ->
      Runtime.(error ~loc (CoercibleExpected v))
 
-let (>>=) = Runtime.bind
 
+(***************************************)
+(* Computations that invoke operations *)
+(***************************************)
+
+(* Maps AML-value-to-(term)-judgment across an option.
+   Fails if given Some value that is not a judgment.
+ *)
 let as_term_option ~loc v =
   match as_option ~loc v with
     | Some v -> Some (Runtime.as_term ~loc v)
     | None -> None
+
+let (>>=) = Runtime.bind
 
 let operation_equal ~loc j1 j2 =
   let v1 = Runtime.mk_term j1
@@ -115,24 +141,35 @@ let operation_coerce_fun ~loc j =
   Runtime.return (as_coercible ~loc v)
 
 let operation_as_prod ~loc j =
-  let v = Runtime.mk_term j in
+  let v = Runtime.mk_term (Jdg.term_of_ty j) in
   Runtime.operation Name.Predefined.as_prod [v] >>= fun v ->
   Runtime.return (as_term_option ~loc v)
 
 let operation_as_eq ~loc j =
-  let v = Runtime.mk_term j in
+  let v = Runtime.mk_term (Jdg.term_of_ty j) in
   Runtime.operation Name.Predefined.as_eq [v] >>= fun v ->
   Runtime.return (as_term_option ~loc v)
 
+(*********)
+(* Other *)
+(*********)
+
+(* Possibly this should be in Runtime?
+   It seems to be here only because predefined_bound_names
+   isn't in the interface.
+*)
+
 let add_abstracting j m =
   let loc = Location.unknown in
+  (* In practice k will be 0 because hypothesis is the first dynamic variable *)
   let k = match Name.level_of_ident Name.Predefined.hypotheses predefined_bound_names with
     | Some k -> k
     | None -> assert false
   in
-  let v = Runtime.mk_term j in
-  Runtime.index_of_level k >>= fun k ->
-  Runtime.lookup_bound ~loc k >>= fun hyps ->
-  let hyps = list_cons v hyps in
-  Runtime.now ~loc k hyps m
+  let v = Runtime.mk_term j in                  (* The given variable as an AML value *)
+  Runtime.index_of_level k >>= fun k ->         (* Switch k from counting from the
+                                                   beginning to counting from the end *)
+  Runtime.lookup_bound ~loc k >>= fun hyps ->   (* Get the AML list of [hypotheses] *)
+  let hyps = list_cons v hyps in                (* Add v to the front of that AML list *)
+  Runtime.now ~loc k hyps m                     (* Run computation m in this dynamic scope *)
 
--- src/runtime/predefined.mli
+++ src/runtime/predefined.mli
@@ -1,27 +1,80 @@
 (** Predefined types and operations. *)
 
+(** The ML equivalent of the AML coercible type
+ *)
 type coercible =
   | NotCoercible
   | Convertible of Jdg.eq_ty
   | Coercible of Jdg.term
 
+(** {6 Built-in Definitions} *)
+
+(** The list of built-in type, operation, and dynamic variable definitions
+    corresponding to the names in Name.Predeclared, e.g.,
+       - ['a list] and its constructors [[]] and [::]
+       - ['a option] and its constructors [Some] and [None]
+       - [coercible] and its constructors (as above)
+       - operations [equal], [as_prod], [as_eq], [coerce], [coerce_fun]
+       - the dynamic variable [hypotheses]
+ *)
 val definitions : Input.toplevel list
 
+(** {6 Runtime computations to invoke operations} *)
+
+(** A computation that, when run, invokes the [equal] operation on the given
+    terms (wrapped as AML values), and then returns the resulting term if any
+    (unwrapped from an AML value to a type theoretical judgment
+ *)
 val operation_equal : loc:Location.t -> Jdg.term -> Jdg.term -> Jdg.term option Runtime.comp
 
+(** A computation that, when run, invokes the [coerce] operation
+    on the given type theory term and desired type, and decodes
+    the resulting AML value as a value of the correponding ML type [coercible].
+ *)
 val operation_coerce : loc:Location.t -> Jdg.term -> Jdg.ty -> coercible Runtime.comp
 
+(** A computation that, when run, invokes the [coerce_fun] operation on the
+    given term and decodes the resulting AML value into the ML type [coercible].
+ *)
 val operation_coerce_fun : loc:Location.t -> Jdg.term -> coercible Runtime.comp
 
-val operation_as_prod : loc:Location.t -> Jdg.term -> Jdg.term option Runtime.comp
-val operation_as_eq : loc:Location.t -> Jdg.term -> Jdg.term option Runtime.comp
+(** A computation that, when run, invokes the [as_prod] operration on the
+    given type; unwraps the resulting evidence (if any) that the given
+    type is equal to a Pi type.
+ *)
+val operation_as_prod : loc:Location.t -> Jdg.ty -> Jdg.term option Runtime.comp
 
+(** A computation that, when run, invokes the [as_eq] operration on the
+    given type; unwraps the resulting evidence (if any) that the given
+    type is equal to an == type.
+ *)
+val operation_as_eq : loc:Location.t -> Jdg.ty -> Jdg.term option Runtime.comp
+
+(** {6 translation between AML and ML values} *)
+
+(** Decodes an AML list value as an ML list of AML values.
+    Fails if the given value is not an AML [list].
+ *)
 val as_list : loc:Location.t -> Runtime.value -> Runtime.value list
+
+(** Encodes a list of AML values as an AML list value
+ *)
 val mk_list : Runtime.value list -> Runtime.value
 
-(** Wrappers for making tags *)
+(** Encodes an ML option as an AML option
+ *)
 val from_option : Runtime.value option -> Runtime.value
+
+(** Decodes an AML option as an ML option.
+    Fails if the given value is not an AML [option]
+ *)
 val as_option : loc:Location.t -> Runtime.value -> Runtime.value option
 
+(** {6 Other} *)
+
+(** Takes a variable (a judgment describing a variable/atom within a context),
+    temporarily adds it to the the front of the list contained in the
+    dynamic variable [hypotheses] to run the given computation.
+ *)
 val add_abstracting : Jdg.term -> 'a Runtime.comp -> 'a Runtime.comp
 
