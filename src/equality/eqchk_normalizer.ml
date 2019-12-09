7df25fc8725e969d4164c22c17c3eebd0ab43c50
Andrej Bauer <Andrej.Bauer@andrej.com>
2019-12-09 14:55:32 +0100

    Setting things up for normalization of heads.

--- src/equality/eqchk_normalizer.ml
+++ src/equality/eqchk_normalizer.ml
@@ -110,6 +110,14 @@ let add_term_computation sgn normalizer drv =
   in
   { normalizer with term_computations = SymbolMap.add sym rls normalizer.term_computations }
 
+let set_type_heads normalizer sym heads =
+  let heads = IntSet.of_list heads in
+  { normalizer with type_heads = SymbolMap.add sym heads normalizer.type_heads }
+
+let set_term_heads normalizer sym heads =
+  let heads = IntSet.of_list heads in
+  { normalizer with term_heads = SymbolMap.add sym heads normalizer.term_heads }
+
 (** Functions that apply rewrite rules *)
 
 (** Find a type computation rule and apply it to [t]. *)
@@ -150,7 +158,6 @@ and apply_term_beta betas sgn e =
        | [] -> None
 
        | (patt, rl) :: lst ->
-          (* XXX match_is_type should normalize parts of t as necessary *)
           begin match Eqchk_pattern.match_is_term sgn e patt with
           | None -> fold lst
           | Some args ->
@@ -165,38 +172,61 @@ and apply_term_beta betas sgn e =
      fold lst
 
 (** Normalize a type *)
-and normalize_type nrm sgn ty =
-  let rec fold ty_eq_ty' ty' =
+and normalize_type nrm sgn ty0 =
+  let rec fold ty0_eq_ty1 ty1 =
+    let ty1_eq_ty2, ty2 = normalize_heads_type nrm sgn ty1 in
+    let ty0_eq_ty2 = Nucleus.transitivity_type ty0_eq_ty1 ty1_eq_ty2 in
 
-    match apply_type_beta nrm.type_computations sgn ty' with
+    match apply_type_beta nrm.type_computations sgn ty2 with
 
-    | None -> ty_eq_ty', Eqchk_common.Normal ty'
+    | None -> ty0_eq_ty2, Eqchk_common.Normal ty2
 
-    | Some ty'_eq_ty'' ->
-       let (Nucleus.Stump_EqType (_, _, ty'')) = Nucleus.invert_eq_type ty'_eq_ty'' in
-       let ty_eq_ty'' = Nucleus.transitivity_type ty_eq_ty' ty'_eq_ty'' in
-       (* XXX normalize heads somewhere *)
-       fold ty_eq_ty'' ty''
+    | Some ty2_eq_ty3 ->
+       let (Nucleus.Stump_EqType (_, _, ty3)) = Nucleus.invert_eq_type ty2_eq_ty3 in
+       let ty0_eq_ty3 = Nucleus.transitivity_type ty0_eq_ty2 ty2_eq_ty3 in
+       fold ty0_eq_ty3 ty3
   in
-  fold (Nucleus.reflexivity_type ty) ty
+  fold (Nucleus.reflexivity_type ty0) ty0
 
 
-and normalize_term nrm sgn e =
-  let rec fold e_eq_e' e' =
+and normalize_term nrm sgn e0 =
+  let rec fold e0_eq_e1 e1 =
+    let e1_eq_e2, e2 = normalize_heads_term nrm sgn e1 in
+    let e0_eq_e2 = Nucleus.transitivity_term e0_eq_e1 e1_eq_e2 in
 
-    match apply_term_beta nrm.term_computations sgn e' with
+    match apply_term_beta nrm.term_computations sgn e2 with
 
-    | None -> e_eq_e', Eqchk_common.Normal e'
+    | None -> e0_eq_e2, Eqchk_common.Normal e2
 
-    | Some e'_eq_e'' ->
-       let (Nucleus.Stump_EqTerm (_, _, e'', _)) = Nucleus.invert_eq_term sgn e'_eq_e'' in
+    | Some e2_eq_e3 ->
+       let (Nucleus.Stump_EqTerm (_, _, e3, _)) = Nucleus.invert_eq_term sgn e2_eq_e3 in
        (* XXX normalize heads somewhere *)
        (* XXX this will fail because e_eq_e' and e'_eq_e'' may happen at different types *)
        (* XXX figure out how to convert e'_eq_e'' using Nucleus.convert_eq_term *)
-       let e_eq_e'' = Nucleus.transitivity_term e_eq_e' e'_eq_e'' in
-       fold e_eq_e'' e''
+       let e0_eq_e3 = Nucleus.transitivity_term e0_eq_e2 e2_eq_e3 in
+       fold e0_eq_e3 e3
   in
-  fold (Nucleus.reflexivity_term sgn e) e
+  fold (Nucleus.reflexivity_term sgn e0) e0
+
+(* Normalize those arguments of [ty0] which are considered to be heads. *)
+and normalize_heads_type nrm sgn ty0 =
+  (* let sym = head_symbol_type (Nucleus.expose_is_type ty0) in *)
+  (* let heads = *)
+  (*   match SymbolMap.find_opt sym nrm.type_heads with *)
+  (*   | None -> IntSet.empty *)
+  (*   | Some heads -> heads *)
+  (* in *)
+  failwith "normalize_heads_type"
+
+(* Normalize those arguments of [e0] which are considered to be heads. *)
+and normalize_heads_term nrm sgn e0 =
+  (* let sym = head_symbol_term (Nucleus.expose_is_term e0) in *)
+  (* let heads = *)
+  (*   match SymbolMap.find_opt sym nrm.term_heads with *)
+  (*   | None -> IntSet.empty *)
+  (*   | Some heads -> heads *)
+  (* in *)
+  failwith "normalize_heads_term"
 
 (* (\** The exported form of normalization for types *\) *)
 (* let normalize_type nrm sgn t = *)
