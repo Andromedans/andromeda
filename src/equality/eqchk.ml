5585915ce72376be03e7c69998ce807d93577e55
Anja Petković <anja.petkovic@gmail.com>
2021-02-26 09:11:38 +0100

    corrections normalization phase

--- src/equality/eqchk.ml
+++ src/equality/eqchk.ml
@@ -83,8 +83,8 @@ and prove_eq_term_abstraction chk sgn abstr =
   fold abstr
 
 and prove_eq_type chk sgn (ty1, ty2) =
-  let ty1_eq_ty1', ty1' = Eqchk_normalizer.normalize_type ~strong:true sgn chk.normalizer ty1
-  and ty2_eq_ty2', ty2' = Eqchk_normalizer.normalize_type ~strong:true sgn chk.normalizer ty2 in
+  let ty1_eq_ty1', ty1' = Eqchk_normalizer.normalize_type ~strong:false sgn chk.normalizer ty1
+  and ty2_eq_ty2', ty2' = Eqchk_normalizer.normalize_type ~strong:false sgn chk.normalizer ty2 in
   let ty1'_eq_ty2' = check_normal_type chk sgn ty1' ty2' in
   Nucleus.transitivity_type
     (Nucleus.transitivity_type ty1_eq_ty1' ty1'_eq_ty2')
@@ -94,8 +94,8 @@ and prove_eq_type chk sgn (ty1, ty2) =
 and prove_eq_term ~ext chk sgn bdry =
   let normalization_phase bdry =
      let (e1, e2, t) = Nucleus.invert_eq_term_boundary bdry in
-     let e1_eq_e1', Normal e1' = Eqchk_normalizer.normalize_term ~strong:true sgn chk.normalizer e1
-     and e2_eq_e2', Normal e2' = Eqchk_normalizer.normalize_term ~strong:true sgn chk.normalizer e2 in
+     let e1_eq_e1', Normal e1' = Eqchk_normalizer.normalize_term ~strong:false sgn chk.normalizer e1
+     and e2_eq_e2', Normal e2' = Eqchk_normalizer.normalize_term ~strong:false sgn chk.normalizer e2 in
      let e1'_eq_e2' = check_normal_term chk sgn (Normal e1') (Normal e2') in
      Nucleus.transitivity_term
        (Nucleus.transitivity_term e1_eq_e1' e1'_eq_e2')
